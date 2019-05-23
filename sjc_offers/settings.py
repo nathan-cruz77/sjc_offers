0b6119d1da1a12e7bc472935e77b86f8cc198fdd
Nathan Cruz <nathan_cruz77@hotmail.com>
2019-05-23 12:27:10 -0300

    Save items to db
    also add docker and docker-compose support

--- sjc_offers/settings.py
+++ sjc_offers/settings.py
@@ -64,9 +64,13 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'sjc_offers.pipelines.SjcOffersPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'sjc_offers.pipelines.MongoPipeline': 300,
+}
+
+# MongoDB credentials
+MONGO_URI = 'mongodb://mongo:27017'
+MONGO_DATABASE = 'app'
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
